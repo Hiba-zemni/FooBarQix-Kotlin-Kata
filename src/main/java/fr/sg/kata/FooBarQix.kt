94ead4e87d853146015134d1fd737b8c759925df
hiba zemni <hibazemni@gmail.com>
2021-03-20 20:18:52 +0100

    useCase with two numbers - first fail test

--- src/main/java/fr/sg/kata/FooBarQix.kt
+++ src/main/java/fr/sg/kata/FooBarQix.kt
@@ -2,7 +2,13 @@ package fr.sg.kata
 
 class FooBarQix {
 
-    fun transformNumber(number : Int):String?{
+
+
+    fun transformNumber(number1 : Int, number2 : Int):String?{
+       return evaluateDivisibleRules(number1)
+    }
+
+    private fun evaluateDivisibleRules (number : Int):String?{
         if(number % 3 == 0)
             return "Foo"
         if(number % 5 == 0)
--- src/test/java/fr/sg/kata/FooBarQixTest.kt
+++ src/test/java/fr/sg/kata/FooBarQixTest.kt
@@ -1,5 +1,6 @@
 package fr.sg.kata
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test
 
@@ -7,32 +8,37 @@ class FooBarQixTest {
 
     @Test
     fun shouldReturn1IfNumberIs1() {
-        assertEquals("1", FooBarQix().transformNumber(1))
+        assertEquals("1", FooBarQix().transformNumber(1, 1))
     }
 
     @Test
     fun shouldReturn2IfNumberIs2() {
-        assertEquals("2", FooBarQix().transformNumber(2))
+        assertEquals("2", FooBarQix().transformNumber(2, 2))
     }
 
     @Test
     fun shouldReturnFooIfNumberI3() {
-        assertEquals("Foo", FooBarQix().transformNumber(3))
+        assertEquals("Foo", FooBarQix().transformNumber(3, 3))
     }
 
     @Test
     fun shouldReturnFooIfNumberI6() {
-        assertEquals("Foo", FooBarQix().transformNumber(6))
+        assertEquals("Foo", FooBarQix().transformNumber(6, 6))
     }
 
     @Test
     fun shouldReturnBarIfNumberI5() {
-        assertEquals("Bar", FooBarQix().transformNumber(5))
+        assertEquals("Bar", FooBarQix().transformNumber(5, 5))
     }
 
     @Test
     fun shouldReturnBarIfNumberI10() {
-        assertEquals("Bar", FooBarQix().transformNumber(10))
+        assertEquals("Bar", FooBarQix().transformNumber(10, 10))
+    }
+
+    @Test
+    fun shouldReturn12IfNumberIsFrom1to2() {
+        assertEquals("12", FooBarQix().transformNumber(1,2))
     }
 
 
